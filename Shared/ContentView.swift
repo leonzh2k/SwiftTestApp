24f4664acb1c3b4be08d9860afccd51fea9dac6e
leonzh18 <leonzh2k@gmail.com>
2022-03-30 15:37:23 -0700

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Leon Zhuang on 3/30/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        let die = "fief"
+        Text(die)
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/appApp.swift
@@ -0,0 +1,17 @@
+//
+//  appApp.swift
+//  Shared
+//
+//  Created by Leon Zhuang on 3/30/22.
+//
+
+import SwiftUI
+
+@main
+struct appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
